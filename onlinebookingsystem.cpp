1b7a0b0cd77381021fd3b479ea3a4e4d5520fc21
maleekanarjisrizvi <117664477+maleekanarjisrizvi@users.noreply.github.com>
2024-01-29 11:59:13 +0500

    Add files via upload

---
+++ onlinebookingsystem.cpp
@@ -0,0 +1,21 @@
+class Customers{
+
+public void searchmodule();
+public void Hoteldetailsmodule(); 
+public void vheckavailability();
+public void payment();
+
+
+
+
+}
+
+
+
+class Hotel
+{
+public void bookrooms(); 
+public void hoteldata();
+public void Bookingdata();
+
+}
